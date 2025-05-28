07b334c7fd7ab8948388f084cc5feb3097ab8488
karthipannerselvam <118588657+karthipannerselvam@users.noreply.github.com>
2025-05-28 08:18:09 +0530

    chenges in backend

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
-const scrapeIMDB = require("./scrape");
+const scrapeIMDB = require("./scraper");
 require("dotenv").config();
 
 const app = express();
